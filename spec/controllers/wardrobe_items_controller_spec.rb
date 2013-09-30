bf03a37d3f9e3a4ba06b6f002a0a0c0e195f0423
Amy Ton <amyhton@gmail.com>
2013-09-30 11:50:07 -0500

    Failed test, yet again

--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -9,4 +9,14 @@ describe WardrobeItemsController do
       expect(assigns('wardrobe_items')).to eq([wardrobe_item])
     end
   end
+  
+  describe "GET show" do
+    it "displays information about a single WardrobeItem as @item" do
+      wardrobe_item = WardrobeItem.create
+      get :show, { :id => wardrobe_item }
+
+      exepct(assigns('wardrobe_itemgit co')).to eq(wardrobe_item)
+    end
+  end
 end
+
